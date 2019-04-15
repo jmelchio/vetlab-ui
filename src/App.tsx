1bbe7472b02c23d41e8d0125d8cec26401b35837
Joris Melchior <joris.melchior@gmail.com>
2019-04-15 01:42:55 -0400

    Experiments with state and forms

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import * as React from 'react';
 import './App.css';
 import { IUser } from './model/User';
 
-export class App extends React.Component {
-  private user: IUser = {
-    adminUser: false,
-    eMail: 'joe.shmoe@yahoo.com',
-    firstName: 'Joe',
-    id: 12345,
-    lastName: 'Shmoe',
-    userName: 'userOne',
-  };
+export interface IAppState {
+  users: IUser[],
+  user: IUser;
+}
+
+export class App extends React.Component<any, IAppState> {
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      user: {
+        adminUser: props.adminUser || false,
+        eMail: props.eMail || '',
+        firstName: props.firstName || '',
+        id: props.id || 0,
+        lastName: props.lastName || '',
+        password: props.password || '',
+        userName: props.userName || '',
+      },
+      users: [],
+    };
+  }
 
   public render() {
     return (
@@ -21,40 +35,118 @@ export class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-        {this.RenderUser(this.user)}
+        <div>
+          {this.RenderForm()}
+        </div>
+        <div>
+          {this.RenderUser(this.state.users)}
+        </div>
       </div>
     );
   }
 
-  private RenderUser = (user: IUser) => {
+  private handleChange = (event: any) => {
+// tslint:disable-next-line: no-console
+    // console.log(event.target.name + ' ' + event.target.value);
+    const user = this.state.user;
+    user[event.target.name] = event.target.value;
+    this.setState({'user': user});
+  }
+
+  private onSubmit = (event: any) => {
+// tslint:disable-next-line: no-console
+    console.log(this.state.user);
+    const user: IUser = {
+        adminUser: this.state.user.adminUser,
+        eMail: this.state.user.eMail,
+        firstName: this.state.user.firstName,
+        id: this.state.user.id,
+        lastName: this.state.user.lastName,
+        password: this.state.user.password,
+        userName: this.state.user.userName,
+    };
+    const users = this.state.users.concat([user]);
+    this.setState({'users': users})
+// tslint:disable-next-line: no-console
+    console.log(users);
+    event.preventDefault();
+  }
+
+  private RenderForm = () => {
+    return (
+      <>
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <div>
+              <label>
+                User Name:
+                <input type="text" value={this.state.user.userName} onChange={this.handleChange} name="userName" />
+              </label>
+            </div>
+            <div>
+              <label>
+                First Name:
+                <input type="text" value={this.state.user.firstName} onChange={this.handleChange} name="firstName" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Last Name:
+                <input type="text" value={this.state.user.lastName} onChange={this.handleChange} name="lastName" />
+              </label>
+            </div>
+            <div>
+              <label>
+                E-mail:
+                <input type="text" value={this.state.user.eMail} onChange={this.handleChange} name="eMail" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Password:
+                <input type="password" value={this.state.user.password} onChange={this.handleChange} name="password" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Admin User:
+                <input type="checkbox" checked={this.state.user.adminUser} onChange={this.handleChange} name="adminUser" />
+              </label>
+            </div>
+            <div>
+              <input type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  }
+
+  private RenderUser = (users: IUser[]) => {
     return (
       <div>
         <table>
-          <tbody>
+          <thead>
             <tr>
               <td>ID</td>
-              <td>{user.id}</td>
-            </tr>
-            <tr>
               <td>User Name</td>
-              <td>{user.userName}</td>
-            </tr>
-            <tr>
               <td>First Name</td>
-              <td>{user.firstName}</td>
-            </tr>
-            <tr>
               <td>Last Name</td>
-              <td>{user.lastName}</td>
-            </tr>
-            <tr>
               <td>E-mail</td>
-              <td>{user.eMail}</td>
-            </tr>
-            <tr>
               <td>Admin User</td>
-              <td>{user.adminUser}</td>
             </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) =>
+              <tr key={index} >
+                <td>{user.id}</td>
+                <td>{user.userName}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.eMail}</td>
+                <td>{user.adminUser}</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
