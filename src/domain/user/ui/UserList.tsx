4181bc7b92e917eda7233b2f6764718e3ae7ec3a
Joris Melchior <joris.melchior@gmail.com>
2020-05-10 10:43:23 -0400

    Add braces to arrow function

--- src/domain/user/ui/UserList.tsx
+++ src/domain/user/ui/UserList.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { IUser } from '../User';
+import * as React from "react";
+import { IUser } from "../User";
 
 export interface IUserListProps {
   users: IUser[];
@@ -23,20 +23,22 @@ export const UserList = (props: IUserListProps): JSX.Element => {
             </tr>
           </thead>
           <tbody>
-            {props.users.map((user, index) =>
-              <tr key={index} >
-                <td>{user.id}</td>
-                <td>{user.userName}</td>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{user.eMail}</td>
-                <td>{user.adminUser}</td>
-                <td>{user.password}</td>
-              </tr>
-            )}
+            {props.users.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{user.id}</td>
+                  <td>{user.userName}</td>
+                  <td>{user.firstName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.eMail}</td>
+                  <td>{user.adminUser}</td>
+                  <td>{user.password}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
     </div>
   );
-}
+};
